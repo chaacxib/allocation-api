0df25cf18ff857e4de31460fc40af54d868724ae
ChaacXib <chaacxib.hi@ud.me>
2023-03-15 15:42:57 -0600

    Base domain model for the project

---
+++ model.py
@@ -0,0 +1,71 @@
+import pydantic
+from datetime import date
+from typing import Optional, List, Set
+
+
+class OutOfStock(Exception):
+    pass
+
+
+class OrderLine(pydantic.BaseModel):
+    sku: str = pydantic.Field(..., example="RED-CHAIR", title="Stock-keeping unit")
+    qty: int = pydantic.Field(..., example=2, title="Quantity")
+    order_id: str = pydantic.Field(..., example="Order1", title="Order ID")
+
+    def __hash__(self) -> int:
+        return hash((type(self),) + tuple(self.__dict__.values()))
+
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]) -> None:
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations: Set[OrderLine] = set()
+
+    def __repr__(self) -> str:
+        return f"<Batch {self.reference}>"
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self) -> int:
+        return hash(self.reference)
+
+    def __gt__(self, other: "Batch") -> bool:
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+    def allocate(self, line: OrderLine) -> None:
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine) -> None:
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    try:
+        batch = next(b for b in sorted(batches) if b.can_allocate(line))
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f"Out of stock for sku {line.sku}")
